83f1745971134a8d2e811646cdd7891e81f59479
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-07-14 12:31:55 -0300

    add details like id, stock, categories in the products detail

--- js/detalle.js
+++ js/detalle.js
@@ -5,13 +5,21 @@ const id = params.get('id')
 const nombreProducto = document.getElementById('nombreProducto')
 const precioProducto = document.getElementById('precioProducto')
 const descripcionProducto = document.getElementById('descripcionProducto')
+const idProducto = document.getElementById('idProducto')
+const categoriaProducto = document.getElementById('categoriaProducto')
+const stockProducto = document.getElementById('stockProducto')
 
 fetch(`${API_URL}/${id}`)
 .then((res) => res.json())
 .then((data) =>{
     nombreProducto.textContent = data.nombre
+    idProducto.textContent = data.id
     precioProducto.textContent = `$${data.precio}`
     descripcionProducto.textContent = data.descripcion 
+    stockProducto.textContent = `${data.stock} unidades`
+    categoriaProducto.textContent = data.categoria.join(' - ').toUpperCase()
+    
+   
 })
 .catch(error  =>{
     console.error({error: `Error al obtener el producto: ${error}`})

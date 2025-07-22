0a459d7d6be78de7383cbd72ac3fad9b041eb955
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-07-22 12:32:53 -0300

    add sweet alert 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const token = localStorage.getItem('token')
 
-if(!token){
+if (!token) {
     window.location.href = './pages/auth.html'
 }
 
@@ -32,36 +32,49 @@ const renderizarProductos = (productos) => {
 
     botonEliminar.forEach((btn) => {
         btn.addEventListener('click', () => {
-            const id = btn.getAttribute('data-id')
+            const id = btn.getAttribute('data-id');
 
-            const confirmDelete = confirm('¿Estas seguro que querés eliminar este producto?');
-
-            if (!confirmDelete) {
-                return
-            }
-
-            fetch(`${API_URL}/${id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                },   
-                method: "DELETE",
-            })
-            .then((res) => res.json())
-            .then(() => {
-                fetch(API_URL)
-                    .then(res => res.json())
-                    .then(data => renderizarProductos(data))
-                    .catch((error) => {
-                        console.error('Error al eliminar producto:', error)
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: "No podrás revertir esto",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                confirmButtonText: 'Sí, eliminar',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(`${API_URL}/${id}`, {
+                        headers: {
+                            Authorization: `Bearer ${token}`
+                        },
+                        method: "DELETE",
                     })
-            })
-        })
+                        .then(res => res.json())
+                        .then(() => {
+                            fetch(API_URL, {
+                                headers: { Authorization: `Bearer ${token}` }
+                            })
+                                .then(res => res.json())
+                                .then(data => renderizarProductos(data));
+
+                            Swal.fire(
+                                'Eliminado',
+                                'El producto fue eliminado correctamente.',
+                                'success'
+                            );
+                        });
+                }
+            });
+        });
+
     })
 
     const botonEditar = document.querySelectorAll('.editar-btn')
 
-    botonEditar.forEach((btn) =>{
-        btn.addEventListener('click', () =>{
+    botonEditar.forEach((btn) => {
+        btn.addEventListener('click', () => {
             const id = btn.getAttribute('data-id')
 
             window.location.href = `./pages/editarProducto.html?id=${id}`
@@ -70,8 +83,8 @@ const renderizarProductos = (productos) => {
 
     const botonDetalle = document.querySelectorAll('.detalle-btn')
 
-    botonDetalle.forEach((btn) =>{
-        btn.addEventListener('click', () =>{
+    botonDetalle.forEach((btn) => {
+        btn.addEventListener('click', () => {
             const id = btn.getAttribute('data-id')
 
             window.location.href = `./pages/verDetalle.html?id=${id}`
@@ -81,9 +94,9 @@ const renderizarProductos = (productos) => {
 
 const filtroCategoria = document.getElementById('filtroCategoria');
 
-filtroCategoria.addEventListener('change', () =>{
+filtroCategoria.addEventListener('change', () => {
     const categoria = filtroCategoria.value;
-    const url = categoria ? `${API_URL}?category=${encodeURIComponent(categoria)}`: API_URL;
+    const url = categoria ? `${API_URL}?category=${encodeURIComponent(categoria)}` : API_URL;
 
     fetch(url, {
         headers: {
@@ -101,38 +114,43 @@ filtroCategoria.addEventListener('change', () =>{
 const formBuscar = document.getElementById('searchProductForm');
 const buscador = document.getElementById('buscador')
 
-formBuscar.addEventListener('submit', (e) =>{
+formBuscar.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const nombreProducto = buscador.value.trim();
-    const url = nombreProducto ? `${API_URL}/search?name=${encodeURIComponent(nombreProducto)}`: API_URL;
+    const url = nombreProducto ? `${API_URL}/search?name=${encodeURIComponent(nombreProducto)}` : API_URL;
 
     fetch(url, {
         headers: {
             Authorization: `Bearer ${token}`
         }
     })
-    .then(res => res.json())
-    .then(data => renderizarProductos(data))
-    .catch(error => {
-        contenedorProductos.innerHTML = "<p>Error al buscar productos.</p>";
-        console.error("Error:", error);
-    });
+        .then(res => res.json())
+        .then(data => renderizarProductos(data))
+        .catch(error => {
+            contenedorProductos.innerHTML = "<p>Error al buscar productos.</p>";
+            console.error("Error:", error);
+        });
 });
 
-fetch(API_URL,{
+fetch(API_URL, {
     headers: {
         Authorization: `Bearer ${token}`
     }
 })
     .then((res) => {
-        if(res.status === 401 || res.status === 403){
+        if (res.status === 401 || res.status === 403) {
             localStorage.removeItem('token');
-            alert('Tu sesión ha expirado. Por favor iniciá sesión nuevamente.');
-            window.location.href = './pages/auth.html';
+            Swal.fire({
+                icon: 'info',
+                title: 'Sesión expirada',
+                text: 'Por favor, iniciá sesión nuevamente.'
+            }).then(() => {
+                window.location.href = './pages/auth.html';
+            });
             return;
-        };
-        
+        }
+
         return res.json()
     })
     .then(data => {
